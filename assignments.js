754bff0211fdc92dc743c4121cc54dc27993c447
Cleary121 <david.cleary@luzern.co>
2026-06-16 21:39:35 +0100

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,75 @@
+// Squads from the live draw (Team List.xlsx). Sits next to index.html.
+window.WC_ASSIGNMENTS = {
+  "p1": [
+    "NED",
+    "NOR",
+    "CZE",
+    "JPN"
+  ],
+  "p2": [
+    "POR",
+    "UZB",
+    "CAN",
+    "NZL"
+  ],
+  "p3": [
+    "BEL",
+    "TUR",
+    "ECU",
+    "RSA"
+  ],
+  "p4": [
+    "MAR",
+    "SEN",
+    "IRN",
+    "HAI"
+  ],
+  "p5": [
+    "BRA",
+    "PAN",
+    "TUN",
+    "IRQ"
+  ],
+  "p6": [
+    "COL",
+    "KSA",
+    "QAT",
+    "CUW"
+  ],
+  "p7": [
+    "CRO",
+    "AUT",
+    "KOR",
+    "EGY"
+  ],
+  "p8": [
+    "ARG",
+    "USA",
+    "PAR",
+    "AUS"
+  ],
+  "p9": [
+    "GER",
+    "SCO",
+    "CIV",
+    "BIH"
+  ],
+  "p10": [
+    "ENG",
+    "GHA",
+    "CPV",
+    "MEX"
+  ],
+  "p11": [
+    "FRA",
+    "JOR",
+    "SWE",
+    "SUI"
+  ],
+  "p12": [
+    "ESP",
+    "COD",
+    "ALG",
+    "URU"
+  ]
+};
